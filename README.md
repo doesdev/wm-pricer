f376c365bf93f91a6337969cb20a6c99843eceb7
Andrew Carpenter <andrew@audioinhd.com>
2016-07-05 20:53:09 -0400

    Change the thing entirely, yays

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # wm-pricer   [![npm version](https://badge.fury.io/js/wm-pricer.svg)](http://badge.fury.io/js/wm-pricer)   [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
 
-An inelegant way to check brickseek for Walmart inventory based on a query to Walmart API. It will open a the brickseek URL for each matching result in your default browser.
-
-Ideally PhantomJS would be employed to automate the filtering process as well as some algorithm's to identify anomalous prices at stores. But I'm not doing that. Pull requests welcomed.
+An inelegant helper for checking brickseek for Walmart inventory based on a query to Walmart API.
 
 ### Requires [Walmart API key](https://developer.walmartlabs.com/member)
 
@@ -11,7 +9,7 @@ Ideally PhantomJS would be employed to automate the filtering process as well as
 
 ## usage
 
-Only one function exported with signature (apiKey, query, start).
+Only one function exported with signature (apiKey, zip, query, start, callback). Callback will be invoked with (err, data) with data being an array of item objects.
 
 The api limits to 25 results each time. So the start arg allows pagination (i.e. for page 2 start at 26).
 
@@ -19,5 +17,5 @@ Provide API key (required)
 ```javascript
 const wmPricer = require('wm-pricer')
 
-wmPricer('someapikey', '4k tv', 1)
+wmPricer('someapikey', 33803, '4k tv', 1, console.log)
 ```
--- index.js
+++ index.js
@@ -2,14 +2,63 @@
 
 // Setup
 const wmApi = require('walmart')
-const open = require('open')
+const http = require('http')
+const cheerio = require('cheerio')
+const keepProps = ['itemId', 'name', 'msrp', 'salePrice', 'modelNumber', 'upc']
 
 // get some stuff and things
-module.exports = (apiKey, query, start) => {
+module.exports = (apiKey, zip, query, start, cb) => {
   let wm = wmApi(apiKey)
-  wm.search(query, {numItems: 25, start}).then((list) => {
-    list.items.forEach((item) => {
-      open(`http://brickseek.com/walmart-inventory-checker?sku=${item.itemId}`)
-    })
+  let out = []
+  let listLength
+  wm.search(query, {numItems: 5, start}).then((list) => {
+    listLength = list.items.length
+    list.items.forEach((item) => getBsHtml(item))
   })
+
+  // get brickseek html
+  function getBsHtml (item) {
+    let reqBody = `zip=${zip}&item_id=${item.itemId}`
+    let reqOpts = {
+      hostname: 'brickseek.com',
+      port: 80,
+      path: `/walmart-inventory-checker?sku=${item.itemId}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(reqBody)
+      }
+    }
+    let body = ''
+    let req = http.request(reqOpts, (res) => {
+      res.setEncoding('utf8')
+      res.on('data', (chunk) => body += chunk)
+      res.on('end', () => processBsResults(item, body))
+    })
+    req.on('error', (err) => cb ? cb(err) : console.error(err))
+    req.write(reqBody)
+    req.end()
+  }
+
+  // process results
+  function processBsResults (wmItem, htmlText) {
+    let $ = cheerio.load(htmlText)
+    let rows = $('#main').find('table').find('tr')
+    let item = {
+      bsUrl: `http://brickseek.com/walmart-inventory-checker?sku=${wmItem.itemId}`
+    }
+    keepProps.forEach((k) => item[k] = wmItem[k])
+    item.inventory = rows.map(function () {
+      let addrHtml = $(this).find('td').html() || ''
+      let priceHtml = $(this).find('a').html() || ''
+      let stockHtml = $(this).find('td').last().html() || ''
+      return {
+        address: addrHtml.replace(/<(\/)?b>/g, '').split('<br>') || [],
+        price: parseFloat(priceHtml.replace('$', '').trim()),
+        stock: parseInt(((stockHtml.split('</b>:') || [])[1] || '').trim(), 10)
+      }
+    }).get().filter((i) => i.price)
+    out.push(item)
+    if (out.length === listLength) return cb ? cb(null, out) : console.log(out)
+  }
 }
