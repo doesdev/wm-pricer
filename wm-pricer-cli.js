479ba3a281ce44c1e0272918f695f2e8d02cb5d7
Andrew Carpenter <andrew@audioinhd.com>
2016-07-07 21:43:04 -0400

    Add min, variance, and min diff to cli

--- wm-pricer-cli.js
+++ wm-pricer-cli.js
@@ -8,9 +8,22 @@ const fs = require('fs')
 const path = require('path')
 const optsAry = ['apiKey', 'query', 'zip', 'limit', 'start']
 const pjOpts = {numberColor: 'yellow'}
+const isNumber = (n) => Number.isFinite(n) && !Number.isNaN(n)
+const propsOrder = [
+  'name',
+  'itemId',
+  'msrp',
+  'salePrice',
+  'minPrice',
+  'variancePercent',
+  'modelNumber',
+  'upc',
+  'bsUrl',
+  'inventory'
+]
 
 // Establish params
-let env, apiKey, query, zip, limit, start, store, fromStored
+let env, apiKey, query, zip, limit, start, store, fromStored, diff
 process.argv.forEach((e, i) => {
   if (e.match(/-r|--remember/)) store = true
   if (e.match(/-k|--apiKey/)) apiKey = process.argv[i + 1]
@@ -18,6 +31,7 @@ process.argv.forEach((e, i) => {
   else if (e.match(/-z|--zip/)) zip = process.argv[i + 1]
   else if (e.match(/-l|--limit/)) limit = process.argv[i + 1]
   else if (e.match(/-s|--start/)) start = process.argv[i + 1]
+  else if (e.match(/-d|--diff/)) diff = parseInt(process.argv[i + 1], 10)
 })
 try {
   env = require('./env.json')
@@ -39,6 +53,20 @@ if (store) {
 }
 // Do that dang thing
 if (!(store && fromStored)) optsAry.forEach((k) => opts[k] = opts[k] || env[k])
-wmPricer(opts, (e, d) => {
-  e ? console.error(e) : console.log(prettyJson.render(d, pjOpts))
+wmPricer(opts, (err, data) => {
+  if (err) return console.error(err)
+  let aryOut = []
+  data.forEach((e, i) => {
+    e.minPrice = Math.min(...(e.inventory || []).map((i) => i.price))
+    e.variancePercent = parseInt(
+      100 - ((e.minPrice / (e.salePrice || e.msrp)) * 100), 10
+    )
+    if (diff && (!isNumber(e.variancePercent) || e.variancePercent < diff)) {
+      return null
+    }
+    aryOut[i] = {}
+    propsOrder.forEach((k) => aryOut[i][k] = e[k])
+  })
+  aryOut.filter((i) => i)
+  console.log(prettyJson.render(aryOut, pjOpts))
 })
