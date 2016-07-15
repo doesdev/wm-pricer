ac99ed37e74ca94377fd3f5d1d653eaeabd5d0f3
Andrew Carpenter <andrew@audioinhd.com>
2016-07-15 15:52:10 -0400

    Expand api, cli

--- index.js
+++ index.js
@@ -1,70 +1,9 @@
-'use strict'
-
 // Setup
-const wmApi = require('walmart')
-const http = require('http')
-const cheerio = require('cheerio')
-const keepProps = ['itemId', 'name', 'msrp', 'salePrice', 'modelNumber', 'upc']
+const WmPricer = require('./lib/wm-pricer')
 
-// get some stuff and things
+// Export
 module.exports = (opts, cb) => {
-  let apiKeyErr = 'API key is required'
-  if (!opts.apiKey) return cb ? cb(apiKeyErr) : console.error(apiKeyErr)
-  let zip = opts.zip || 33803
-  let query = opts.query || '4k tv'
-  let limit = opts.limit || 25
-  let start = opts.start || 1
-  let wm = wmApi(opts.apiKey)
-  let out = []
-  let listLength
-  wm.search(query, {numItems: limit, start}).then((list) => {
-    listLength = list.items.length
-    list.items.forEach((item) => getBsHtml(item))
-  })
-
-  // get brickseek html
-  function getBsHtml (item) {
-    let reqBody = `zip=${zip}&item_id=${item.itemId}`
-    let reqOpts = {
-      hostname: 'brickseek.com',
-      port: 80,
-      path: `/walmart-inventory-checker?sku=${item.itemId}`,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(reqBody)
-      }
-    }
-    let body = ''
-    let req = http.request(reqOpts, (res) => {
-      res.setEncoding('utf8')
-      res.on('data', (chunk) => body += chunk)
-      res.on('end', () => processBsResults(item, body))
-    })
-    req.on('error', (err) => cb ? cb(err) : console.error(err))
-    req.write(reqBody)
-    req.end()
-  }
-
-  // process results
-  function processBsResults (wmItem, htmlText) {
-    let $ = cheerio.load(htmlText)
-    let rows = $('#main').find('table').find('tr')
-    let item = {
-      bsUrl: `http://brickseek.com/walmart-inventory-checker?sku=${wmItem.itemId}`
-    }
-    keepProps.forEach((k) => item[k] = wmItem[k])
-    item.inventory = rows.map(function () {
-      let addrHtml = $(this).find('td').html() || ''
-      let priceHtml = $(this).find('a').html() || ''
-      let stockHtml = $(this).find('td').last().html() || ''
-      return {
-        address: addrHtml.replace(/<(\/)?b>/g, '').split('<br>') || [],
-        price: parseFloat(priceHtml.replace('$', '').trim()),
-        stock: parseInt(((stockHtml.split('</b>:') || [])[1] || '').trim(), 10)
-      }
-    }).get().filter((i) => i.price)
-    out.push(item)
-    if (out.length === listLength) return cb ? cb(null, out) : console.log(out)
-  }
+  let wmPricer = WmPricer.new(opts)
+  wmPricer.brickseek(opts, cb)
 }
+module.exports.api = WmPricer
--- wm-pricer-cli.js
+++ wm-pricer-cli.js
@@ -35,6 +35,7 @@ options:
   -z, --zip       Zip code to search within (50 mile radius)
   -l, --limit     Number of results to return (up to 25, limited by WM API)
   -p, --page      Result set page to return
+  -s, --store     Search using storeQuery api
   -d, --diff      In store price difference threshold
     (i.e. only show results with X percent lower price in store than online)
 
@@ -48,55 +49,68 @@ If apiKey and zip is stored and no other options supplied you can call with
   ~ wmp query
 `
 
-// Establish params
-let env, apiKey, query, zip, limit, page, store, fromStored, diff
-process.argv.forEach((e, i) => {
-  if (e.match(/-h|--help/)) console.log(man)
-  if (e.match(/-r|--remember/)) store = true
-  if (e.match(/-k|--apiKey/)) apiKey = process.argv[i + 1]
-  else if (e.match(/-q|--query/)) query = process.argv[i + 1]
-  else if (e.match(/-z|--zip/)) zip = process.argv[i + 1]
-  else if (e.match(/-l|--limit/)) limit = parseInt(process.argv[i + 1], 10)
-  else if (e.match(/-p|--page/)) page = parseInt(process.argv[i + 1], 10)
-  else if (e.match(/-d|--diff/)) diff = parseInt(process.argv[i + 1], 10)
-})
-try {
-  env = require('./env.json')
-  fromStored = true
-} catch (e) {
-  env = process.env || {}
-}
-if (!(apiKey || query || zip || limit || page)) {
-  let firstIsNum = process.argv[2].match(/^\d+$/)
-  zip = process.argv[firstIsNum ? 2 : 3]
-  query = process.argv[firstIsNum ? 3 : 2]
-}
-let start = ((page || 1) * (limit || 25) + 1) - (limit || 25)
-const opts = {apiKey, query, zip, limit, start}
-
-// Store vars if that flag is set
-if (store) {
-  if (fromStored) optsAry.forEach((k) => opts[k] = opts[k] || env[k])
-  fs.writeFileSync(path.resolve(__dirname, 'env.json'), JSON.stringify(opts))
-}
-// Do that dang thing
-if (!(store && fromStored)) optsAry.forEach((k) => opts[k] = opts[k] || env[k])
-wmPricer(opts, (err, data) => {
-  if (err) return console.error(err)
-  let aryOut = []
-  data.forEach((e, i) => {
-    e.minPrice = Math.min(...(e.inventory || []).map((i) => i.price))
-    e.variancePercent = parseInt(
-      100 - ((e.minPrice / (e.salePrice || e.msrp)) * 100), 10
-    )
-    if (diff && (!isNumber(e.variancePercent) || e.variancePercent < diff)) {
-      return null
-    }
-    e.inventory.sort((a, b) => a.price - b.price)
-    aryOut[i] = {}
-    propsOrder.forEach((k) => aryOut[i][k] = e[k])
+let init = () => {
+  // Establish params
+  let env, apiKey, query, zip, limit, page, store, fromStored, diff, sq, help
+  process.argv.forEach((e, i) => {
+    if (e.match(/-h|--help/)) return (help = true)
+    if (e.match(/-r|--remember/)) store = true
+    if (e.match(/-k|--apiKey/)) apiKey = process.argv[i + 1]
+    else if (e.match(/-q|--query/)) query = process.argv[i + 1]
+    else if (e.match(/-z|--zip/)) zip = process.argv[i + 1]
+    else if (e.match(/-l|--limit/)) limit = parseInt(process.argv[i + 1], 10)
+    else if (e.match(/-p|--page/)) page = parseInt(process.argv[i + 1], 10)
+    else if (e.match(/-s|--store/)) sq = true
+    else if (e.match(/-d|--diff/)) diff = parseInt(process.argv[i + 1], 10)
   })
-  aryOut = aryOut.filter((i) => i)
-  aryOut.sort((a, b) => b.variancePercent - a.variancePercent)
-  console.log(prettyJson.render(aryOut, pjOpts))
-})
+  if (help) return console.log(man)
+  try {
+    env = require('./env.json')
+    fromStored = true
+  } catch (e) {
+    env = process.env || {}
+  }
+  if (!(apiKey || query || zip || limit || page)) {
+    let firstIsNum = process.argv[2].match(/^\d+$/)
+    zip = process.argv[firstIsNum ? 2 : 3]
+    query = process.argv[firstIsNum ? 3 : 2]
+  }
+  let start = ((page || 1) * (limit || 25) + 1) - (limit || 25)
+  const opts = {apiKey, query, zip, limit, start}
+
+  // Store vars if that flag is set
+  if (store) {
+    if (fromStored) optsAry.forEach((k) => opts[k] = opts[k] || env[k])
+    fs.writeFileSync(path.resolve(__dirname, 'env.json'), JSON.stringify(opts))
+  }
+  // Do that dang thing
+  if (!(store && fromStored)) optsAry.forEach((k) => opts[k] = opts[k] || env[k])
+  if (sq) {
+    if (diff) opts.minDiff = diff
+    let wmp = wmPricer.api.new(opts)
+    wmp.on('store-query-done', (d) => console.log(prettyJson.render(d, pjOpts)))
+    wmp.storeQuery(opts)
+    return
+  }
+  wmPricer(opts, (err, data) => {
+    if (err) return console.error(err)
+    let aryOut = []
+    data.forEach((e, i) => {
+      e.minPrice = Math.min(...(e.inventory || []).map((i) => i.price))
+      e.variancePercent = parseInt(
+        100 - ((e.minPrice / (e.salePrice || e.msrp)) * 100), 10
+      )
+      if (diff && (!isNumber(e.variancePercent) || e.variancePercent < diff)) {
+        return null
+      }
+      e.inventory.sort((a, b) => a.price - b.price)
+      aryOut[i] = {}
+      propsOrder.forEach((k) => aryOut[i][k] = e[k])
+    })
+    aryOut = aryOut.filter((i) => i)
+    aryOut.sort((a, b) => b.variancePercent - a.variancePercent)
+    console.log(prettyJson.render(aryOut, pjOpts))
+  })
+}
+
+init()
